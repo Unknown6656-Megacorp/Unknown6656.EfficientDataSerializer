ef1d09aa9160fb710c8e825165975a85ec41d684
unknown6656 <unknown84@gmx.de>
2023-05-28 15:23:32 +0200

    added helper classes

---
+++ Unknown6656.EDS.CSharp/Internals/CompressedStringDictionary.cs
@@ -0,0 +1,100 @@
+﻿namespace Unknown6656.EDS.Internals;
+
+
+internal sealed class CompressedStringDictionary
+{
+    private const char MAP_START = '\xe000';
+    private const char MAP_END = '\xf8ff';
+
+    public bool IgnoreCase { get; }
+    public string[] SourceStrings { get; }
+    public string[] Prefixes { get; }
+    public string[] Mapping { get; }
+
+
+    private CompressedStringDictionary(IEnumerable<string> prefixes)
+    {
+        IgnoreCase = false;
+        Prefixes = prefixes.ToArray();
+        SourceStrings = Array.Empty<string>();
+        Mapping = Array.Empty<string>();
+    }
+
+    public CompressedStringDictionary(IEnumerable<string> strings, bool ignore_case)
+    {
+        IgnoreCase = ignore_case;
+        SourceStrings = strings.ToArray();
+        (Prefixes, Mapping) = BuildMapping();
+    }
+
+    private (string[] prefixes, string[] mapping) BuildMapping()
+    {
+        string[] mapping = new string[SourceStrings.Length];
+        Dictionary<string, HashSet<int>> prefix_map = new();
+
+        for (int i = 0; i < mapping.Length; ++i)
+        {
+            foreach (char c in SourceStrings[i])
+                if (c >= MAP_START && c <= MAP_END)
+                    throw new NotSupportedException($"The string '{SourceStrings[i]}' contains the character 0x{(int)c:x4} which is reserved for internal usage.");
+
+            for (int j = i + 1; j < mapping.Length; ++j)
+                if (CommonPrefix(SourceStrings[i], SourceStrings[j]) is { Length: > 1 } prefix)
+                {
+                    if (!prefix_map.TryGetValue(prefix, out HashSet<int>? indices))
+                        prefix_map[prefix] = indices = new() { i };
+
+                    indices.Add(j);
+                }
+        }
+
+        string[] prefixes = prefix_map.Keys.OrderByDescending(p => p.Length).ToArray();
+
+        if (prefixes.Length > MAP_END - MAP_START)
+            throw new NotSupportedException($"The dictionary contains more than {MAP_END - MAP_START} unique prefixes and can therefore not be efficiently compressed");
+
+        for (int i = 0; i < mapping.Length; ++i)
+        {
+            string s = SourceStrings[i];
+
+            for (int j = 0; j < prefixes.Length; ++j)
+                if (prefix_map[prefixes[j]].Contains(i))
+                {
+                    s = (char)(MAP_START + j) + s[prefixes[j].Length..];
+
+                    break;
+                }
+
+            mapping[i] = s;
+        }
+
+        return (prefixes, mapping);
+    }
+
+    public string GetMapping(string source)
+    {
+        for (int i = 0; i < SourceStrings.Length; ++i)
+            if (SourceStrings[i].Equals(source, IgnoreCase ? StringComparison.InvariantCultureIgnoreCase : StringComparison.InvariantCulture))
+                return Mapping[i];
+
+        throw new KeyNotFoundException();
+    }
+
+    public string FromMapping(string mapping) =>
+        mapping.Length > 0 && mapping[0] is char pfx and >= MAP_START and <= MAP_END ? Prefixes[pfx - MAP_START] + mapping[1..] : mapping;
+
+    private string CommonPrefix(string first, string second)
+    {
+        int length = 0;
+
+        foreach (char c in first)
+            if (second.Length <= length || (IgnoreCase ? char.ToLowerInvariant(second[length]) != char.ToLowerInvariant(c) : second[length] != c))
+                return first[..length];
+            else
+                ++length;
+
+        return first;
+    }
+
+    internal static CompressedStringDictionary FromPrefixes(IEnumerable<string> prefixes) => new(prefixes);
+}
---
+++ Unknown6656.EDS.CSharp/Internals/MinimalPairFinder.cs
@@ -0,0 +1,69 @@
+﻿namespace Unknown6656.EDS.Internals;
+
+
+internal class MinimalPairFinder<X, Y>
+    where X : notnull
+{
+    public Func<X, Y, int> Distance { get; }
+
+
+    public MinimalPairFinder(Func<X, Y, int> distance) => Distance = distance;
+
+    public Dictionary<X, Y> FindMinimalPairs(IEnumerable<X> x, IEnumerable<Y> y) => FindMinimalPairs(x, y, out _);
+
+    public Dictionary<X, Y> FindMinimalPairs(IEnumerable<X> x, IEnumerable<Y> y, out int sum)
+    {
+        X[] xs = x as X[] ?? x.ToArray();
+        Y[] ys = y as Y[] ?? y.ToArray();
+        int n = xs.Length;
+        int m = ys.Length;
+        int[,] matrix = new int[n, m];
+
+        Parallel.For(0, n * m, i => matrix[i % n, i / n] = Distance(xs[i % n], ys[i / n]));
+
+        (int x, int y)[] pairs = FindMinimalPairs(matrix, n, m);
+        sum = pairs.Sum(t => matrix[t.x, t.y]);
+
+        return pairs.ToDictionary(t => xs[t.x], t => ys[t.y]);
+    }
+
+    private static (int x, int y)[] FindMinimalPairs(int[,] matrix, int N, int M)
+    {
+        int L = Math.Min(N, M);
+        List<(int x, int y)> pairs = new();
+        List<int> columns = Enumerable.Range(0, M).ToList();
+        List<int> rows = Enumerable.Range(0, N).ToList();
+
+        for (int i = 0; i < L; ++i)
+        {
+            int min_sum = int.MaxValue;
+            int sel_row = -1;
+            int sel_col = -1;
+
+            foreach (int row in rows)
+                foreach (int column in columns)
+                    if (matrix[row, column] + pairs.Sum(t => matrix[t.x, t.y]) is int sum && sum < min_sum)
+                    {
+                        min_sum = sum;
+                        sel_row = row;
+                        sel_col = column;
+                    }
+
+            pairs.Add((sel_row, sel_col));
+            columns.Remove(sel_col);
+            rows.Remove(sel_row);
+        }
+
+        return pairs.ToArray();
+    }
+}
+
+internal class MinimalPairFinder<X>
+    : MinimalPairFinder<X, X>
+    where X : notnull
+{
+    public MinimalPairFinder(Func<X, X, int> distance)
+        : base(distance)
+    {
+    }
+}
